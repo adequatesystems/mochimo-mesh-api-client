632227046f3b1b6703927f709bafd9d9056bfe35
awagl <anujwagle@gmail.com>
2025-01-15 03:51:43 -0500

    change export name of client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { TransactionBuilder, type TransactionParams } from './transaction';
-export { MochimoApiClient as MochimoConstruction } from './api';
+export { MochimoApiClient } from './api';
 export { formatMemo, isValidMemo } from './utils/memo';
 export * from './types'; 
